e1b69270f373ea7aea49122b6644ed642e7acd59
Kavindu Piyumantha <tkp99x@gmail.com>
2024-10-06 06:24:56 +0530

    generate analysis report generation add

--- apk_analysis/views.py
+++ apk_analysis/views.py
@@ -20,18 +20,25 @@ class APKUploadView(APIView):
         file_path = default_storage.save(file.name, file)
         abs_path = os.path.join(settings.MEDIA_ROOT, file_path)
 
-        # Perform analysis with MobSF
+        # Perform analysis with MobSF - upload APK
         upload_response = self.upload_to_mobsf(abs_path, file.name)
         if "error" in upload_response:
             return Response(upload_response, status=status.HTTP_400_BAD_REQUEST)
 
-        # Use the hash to perform the scan
-        analysis_result = self.scan_with_mobsf(upload_response['hash'])
+        # Perform scan with MobSF using the hash obtained
+        scan_response = self.scan_with_mobsf(upload_response['hash'])
+        if "error" in scan_response:
+            return Response(scan_response, status=status.HTTP_400_BAD_REQUEST)
+
+        # Generate JSON report with MobSF using the hash
+        json_report_response = self.generate_json_report(upload_response['hash'])
+        if "error" in json_report_response:
+            return Response(json_report_response, status=status.HTTP_400_BAD_REQUEST)
 
         # Save analysis result to database
         apk_analysis = APKAnalysis.objects.create(
             file_name=file.name,
-            analysis_result=analysis_result
+            analysis_result=json_report_response
         )
         serializer = APKAnalysisSerializer(apk_analysis)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -90,3 +97,31 @@ class APKUploadView(APIView):
         except Exception as e:
             # Handle any other errors
             return {"error": f"An exception occurred during file scan: {str(e)}"}
+
+    def generate_json_report(self, file_hash):
+        try:
+            # Headers for MobSF API
+            headers = {
+                'Authorization': settings.MOBSF_API_KEY
+            }
+
+            # JSON Report API URL
+            json_report_url = f"{settings.MOBSF_API_URL}/api/v1/report_json"
+
+            # Data to request JSON report
+            data = {
+                'hash': file_hash
+            }
+
+            # Send request to MobSF to generate the JSON report
+            response = requests.post(json_report_url, headers=headers, data=data)
+
+            if response.status_code == 200:
+                # Successful response - return the JSON report
+                return response.json()
+            else:
+                # Handle error from MobSF API
+                return {"error": f"MobSF API error during JSON report generation: {response.text}"}
+        except Exception as e:
+            # Handle any other errors
+            return {"error": f"An exception occurred during JSON report generation: {str(e)}"}
